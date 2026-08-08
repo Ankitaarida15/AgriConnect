c694e67984ccca4fb0ceca8c83df3ef54fa177a4
Ankita Arida <ankitaarida115@example.com>
2026-08-08 17:54:50 +0530

    Add profile user API

--- backend/server.js
+++ backend/server.js
@@ -243,6 +243,42 @@ app.post("/login", async (req, res) => {
 });
 
 // =====================
+// GET LOGGED-IN USER
+// =====================
+app.get("/me", authMiddleware, async (req, res) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: req.user.id,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        phone: true,
+        village: true,
+        role: true,
+        createdAt: true,
+      },
+    });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    res.status(200).json(user);
+
+  } catch (error) {
+    console.error("ME ERROR:", error);
+
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+});
+// =====================
 // GET ALL PRODUCTS
 // =====================
 app.get("/products", async (req, res) => {
